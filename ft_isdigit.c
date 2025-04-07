5c6353832bbc1ddb3a837aab004ea1f01d78a30f
Ignacio Oronoz Hernández <87530036+rainnsy@users.noreply.github.com>
2025-04-07 17:12:43 +0200

    All things i do outter 42 Madrid (MY HOUSE)

---
+++ ft_isdigit.c
@@ -0,0 +1,6 @@
+int ft_isdigit(int num)
+{
+    if (num >= 48 && num <= 57)
+        return (1);
+    return(0);
+}
---
+++ ft_strncmp.c
@@ -0,0 +1,13 @@
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+    int i;
+
+    i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n)
+        i++;
+
+    if (i < n)
+        return (s1[i] - s2[i]);
+    else
+        return (0);
+}

451439e814b35ab9ad85bfb00f0572565439d99e
Ignacio Oronoz Hernández <nachooronoz11@gmail.com>
2025-05-09 13:17:04 +0200

    Ready for the tests!

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,4 +1,4 @@
-#include "libft.h"
+# include "libft.h"
 
 int ft_count_digits(int n)
 {
@@ -20,5 +20,29 @@ int ft_count_digits(int n)
 char *ft_itoa(int n)
 {
 	char	*c;
+	int		len;
+	int		sign;
+	long	num;
 
+	sign = 1;
+	len = ft_count_digits(n);
+	c = (char *)malloc(sizeof(char) * (len + 1));
+	if (!c)
+		return (NULL);
+	c[len] = '\0';
+	if (n < 0)
+	{
+		sign = -1;
+		c[0] = '-';
+	}
+	num = n;
+	if (sign == -1)
+		num = -num;
+	while (len > 0 && c[len - 1] != '-')
+	{
+		len--;
+		c[len] = (num % 10) + '0';
+		num /= 10;
+	}
+	return (c);
 }
